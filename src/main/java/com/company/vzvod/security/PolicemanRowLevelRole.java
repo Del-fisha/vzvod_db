79af6eb9fe8c5fa116e52568e8f4582e6be08015
Tarasov Aleksey <del-fisha@yandex.ru>
2026-02-12 11:33:47 +0300

    fixed

--- src/main/java/com/company/vzvod/security/PolicemanRowLevelRole.java
+++ src/main/java/com/company/vzvod/security/PolicemanRowLevelRole.java
@@ -1,18 +1,18 @@
-package com.company.vzvod.security;
-
-import com.company.vzvod.entity.User;
-import io.jmix.security.role.annotation.JpqlRowLevelPolicy;
-import io.jmix.security.role.annotation.RowLevelRole;
-
-@RowLevelRole(name = "PolicemanRowLevel", code = PolicemanRowLevelRole.CODE)
-public interface PolicemanRowLevelRole {
-    String CODE = "policeman-row-level";
-
-    @JpqlRowLevelPolicy(
-            entityClass = User.class,
-            where = "{E}.id = :current_user_id"
-
-    )
-    void userUpdateOnlySelf();
-
-}
+//package com.company.vzvod.security;
+//
+//import com.company.vzvod.entity.User;
+//import io.jmix.security.role.annotation.JpqlRowLevelPolicy;
+//import io.jmix.security.role.annotation.RowLevelRole;
+//
+//@RowLevelRole(name = "PolicemanRowLevel", code = PolicemanRowLevelRole.CODE)
+//public interface PolicemanRowLevelRole {
+//    String CODE = "policeman-row-level";
+//
+//    // Ограничиваем только UPDATE своей записью
+//    @JpqlRowLevelPolicy(
+//            entityClass = User.class,
+//            where = "{E}.id = :current_user_id"
+//    )
+//    void userUpdateOnlySelf();
+//
+//}
