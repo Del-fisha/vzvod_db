f17c9c070c7841575d1f23cc72ee22334500c973
Tarasov Aleksey <del-fisha@yandex.ru>
2026-02-24 16:03:11 +0300

    added Vocation test

--- src/test/java/com/company/vzvod/entity/VocationTest.java
+++ src/test/java/com/company/vzvod/entity/VocationTest.java
@@ -3,6 +3,9 @@ package com.company.vzvod.entity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class VocationTest extends EntityTestSupport {
@@ -16,34 +19,79 @@ class VocationTest extends EntityTestSupport {
 
     @Test
     void testUserServiceInfo() {
+        assertNull(vocation.getUserServiceInfo());
+
+        ServiceInfo serviceInfo = dataManager.create(ServiceInfo.class);
+
+        vocation.setUserServiceInfo(serviceInfo);
+        assertSame(serviceInfo, vocation.getUserServiceInfo());
     }
 
     @Test
     void testType() {
+        assertNull(vocation.getType());
+
+        vocation.setType(VocationType.MAIN);
+        assertSame(VocationType.MAIN, vocation.getType());
     }
 
     @Test
     void testStartDate() {
+        assertNull(vocation.getStartDate());
+
+        LocalDate date = LocalDate.now();
+        vocation.setStartDate(date);
+
+        assertSame(date, vocation.getStartDate());
     }
 
     @Test
     void testEndDate() {
+        assertNull(vocation.getEndDate());
+
+        LocalDate date = LocalDate.now();
+        vocation.setEndDate(date);
+
+        assertSame(date, vocation.getEndDate());
     }
 
     @Test
     void testCountOfDays() {
+        assertNull(vocation.getCountOfDays());
+
+        int countOfDays = 40;
+        vocation.setCountOfDays(countOfDays);
+
+        assertEquals(countOfDays, (int) vocation.getCountOfDays());
     }
 
     @Test
     void testAllRemaindDays() {
+        assertNull(vocation.getAllRemaindDays());
+
+        int allRemainedDays = 10;
+        vocation.setAllRemaindDays(10);
+
+        assertEquals(allRemainedDays, vocation.getAllRemaindDays());
     }
 
     @Test
     void testHasDeparture() {
+        assertNull(vocation.isHasDeparture());
+
+        Boolean hasDeparture = true;
+        vocation.setHasDeparture(hasDeparture);
+
+        assertEquals(hasDeparture, vocation.isHasDeparture());
     }
 
     @Test
     void testCityToDrive() {
+        assertNull(vocation.getCityToDrive());
+
+        String city = "Москва";
+        vocation.setCityToDrive(city);
+        assertEquals(city, vocation.getCityToDrive());
     }
 
     @Test
@@ -52,5 +100,11 @@ class VocationTest extends EntityTestSupport {
 
     @Test
     void testId() {
+        UUID originalId = vocation.getId();
+        assertNotNull(originalId);
+
+        UUID newId = UUID.randomUUID();
+        vocation.setId(newId);
+        assertEquals(newId, vocation.getId());
     }
 }
