2ba4c1559d39c14a362502a9ca1ddd4715300e71
Tarasov Aleksey <del-fisha@yandex.ru>
2026-02-24 14:58:38 +0300

    added Vocation test layout

---
+++ src/test/java/com/company/vzvod/entity/VocationTest.java
@@ -0,0 +1,56 @@
+package com.company.vzvod.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VocationTest extends EntityTestSupport {
+
+    Vocation vocation;
+
+    @BeforeEach
+    void setUp() {
+        vocation = dataManager.create(Vocation.class);
+    }
+
+    @Test
+    void testUserServiceInfo() {
+    }
+
+    @Test
+    void testType() {
+    }
+
+    @Test
+    void testStartDate() {
+    }
+
+    @Test
+    void testEndDate() {
+    }
+
+    @Test
+    void testCountOfDays() {
+    }
+
+    @Test
+    void testAllRemaindDays() {
+    }
+
+    @Test
+    void testHasDeparture() {
+    }
+
+    @Test
+    void testCityToDrive() {
+    }
+
+    @Test
+    void testDaysAddedByDeparture() {
+    }
+
+    @Test
+    void testId() {
+    }
+}
