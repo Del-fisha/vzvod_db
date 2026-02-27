b087a32b79d2b94c4e8e1ecab7d38cd182256875
Tarasov Aleksey <del-fisha@yandex.ru>
2026-02-27 13:46:11 +0300

    done

--- src/main/java/com/company/vzvod/service/PhoneNormalizer.java
+++ src/main/java/com/company/vzvod/service/PhoneNormalizer.java
@@ -3,7 +3,9 @@ package com.company.vzvod.service;
 public class PhoneNormalizer {
 
     public String normalize(String phone) {
-        if (phone.length() == 11) {
+        if (phone.length() == 12) {
+            return phone;
+        } else if (phone.length() == 11) {
             phone = phone.substring(1, 11);
         }
         return "+7".concat(phone);
