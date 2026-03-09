8d35358ba41785a2531cf6a3e0ab314e6a827315
Tarasov Aleksey <del-fisha@yandex.ru>
2026-03-09 17:49:06 +0300

    changed in dep

--- src/main/java/com/company/vzvod/view/department/DepartmentListView.java
+++ src/main/java/com/company/vzvod/view/department/DepartmentListView.java
@@ -1,12 +1,13 @@
 package com.company.vzvod.view.department;
 
 import com.company.vzvod.entity.Department;
+import com.company.vzvod.view.main.MainView;
 import com.company.vzvod.view.mainviewtopmenu.MainViewTopMenu;
 import com.vaadin.flow.router.Route;
 import io.jmix.flowui.view.*;
 
 
-@Route(value = "departments", layout = MainViewTopMenu.class)
+@Route(value = "departments", layout = MainView.class)
 @ViewController(id = "Department.list")
 @ViewDescriptor(path = "department-list-view.xml")
 @LookupComponent("departmentsDataGrid")
