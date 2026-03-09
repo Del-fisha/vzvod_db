f78e30b146b9016d7270cdae6a65597b81377390
Tarasov Aleksey <del-fisha@yandex.ru>
2026-03-09 17:50:59 +0300

    changed in serviceInfo

--- src/main/java/com/company/vzvod/view/serviceinfo/ServiceInfoListView.java
+++ src/main/java/com/company/vzvod/view/serviceinfo/ServiceInfoListView.java
@@ -1,12 +1,13 @@
 package com.company.vzvod.view.serviceinfo;
 
 import com.company.vzvod.entity.ServiceInfo;
+import com.company.vzvod.view.main.MainView;
 import com.company.vzvod.view.mainviewtopmenu.MainViewTopMenu;
 import com.vaadin.flow.router.Route;
 import io.jmix.flowui.view.*;
 
 
-@Route(value = "service-infoes", layout = MainViewTopMenu.class)
+@Route(value = "service-infoes", layout = MainView.class)
 @ViewController(id = "ServiceInfo.list")
 @ViewDescriptor(path = "service-info-list-view.xml")
 @LookupComponent("serviceInfoesDataGrid")
