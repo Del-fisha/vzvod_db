0a3b84478aa4e3e2a2cd66220a9fbaac1d8aa511
Tarasov Aleksey <del-fisha@yandex.ru>
2026-04-21 20:42:49 +0300

    created

--- src/main/java/com/company/vzvod/view/administrativeviolation/AdministrativeViolationListView.java
+++ src/main/java/com/company/vzvod/view/administrativeviolation/AdministrativeViolationListView.java
@@ -1,10 +1,13 @@
 package com.company.vzvod.view.administrativeviolation;
 
 import com.company.vzvod.entity.AdministrativeViolation;
+import com.company.vzvod.entity.ServiceInfo;
 import com.company.vzvod.view.main.MainView;
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
 
+import java.util.Objects;
 
 @Route(value = "administrative-violations", layout = MainView.class)
 @ViewController(id = "AdministrativeViolation.list")
@@ -12,4 +15,19 @@ import io.jmix.flowui.view.*;
 @LookupComponent("administrativeViolationsDataGrid")
 @DialogMode(width = "64em")
 public class AdministrativeViolationListView extends StandardListView<AdministrativeViolation> {
+
+    @ViewComponent
+    private CollectionLoader<AdministrativeViolation> administrativeViolationsDl;
+
+    private ServiceInfo serviceInfo;
+
+    public void setServiceInfo(ServiceInfo serviceInfo) {
+        this.serviceInfo = serviceInfo;
+    }
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        administrativeViolationsDl.setParameter("serviceInfo", serviceInfo);
+        administrativeViolationsDl.load();
+    }
 }
