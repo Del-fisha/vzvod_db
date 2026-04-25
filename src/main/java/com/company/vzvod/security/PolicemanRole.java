e8b06f01aa7c9a4432fcedc23574442dce122b01
Tarasov Aleksey <del-fisha@yandex.ru>
2026-04-25 17:58:20 +0300

    now PolicemanRole = FullAccessRole, without '*'

--- src/main/java/com/company/vzvod/security/PolicemanRole.java
+++ src/main/java/com/company/vzvod/security/PolicemanRole.java
@@ -7,6 +7,7 @@ import io.jmix.security.model.SecurityScope;
 import io.jmix.security.role.annotation.EntityAttributePolicy;
 import io.jmix.security.role.annotation.EntityPolicy;
 import io.jmix.security.role.annotation.ResourceRole;
+import io.jmix.security.role.annotation.SpecificPolicy;
 import io.jmix.securityflowui.role.annotation.MenuPolicy;
 import io.jmix.securityflowui.role.annotation.ViewPolicy;
 
@@ -14,148 +15,254 @@ import io.jmix.securityflowui.role.annotation.ViewPolicy;
 public interface PolicemanRole {
     String CODE = "policeman-role";
 
-    @EntityPolicy(entityClass = User.class, actions = {EntityPolicyAction.READ})
-    void userEntity();
-
-    @EntityPolicy(entityClass = ServiceInfo.class, actions = {EntityPolicyAction.READ, EntityPolicyAction.UPDATE})
-    void serviceEntity();
-
-    @EntityPolicy(entityClass = IdCard.class, actions = {EntityPolicyAction.READ, EntityPolicyAction.CREATE, EntityPolicyAction.UPDATE})
-    void idCardEntity();
-
-    @EntityPolicy(entityClass = Penalty.class, actions = {EntityPolicyAction.READ})
-    void penaltyEntity();
-
-    @EntityPolicy(entityClass = Incentive.class, actions = {EntityPolicyAction.READ})
-    void incentiveEntity();
-
-    @EntityPolicy(entityClass = Shift.class, actions = {EntityPolicyAction.READ})
-    void shiftEntity();
-
-    @EntityPolicy(entityClass = Vocation.class, actions = {EntityPolicyAction.READ})
-    void vocationEntity();
-
-    @EntityPolicy(entityClass = Contacts.class, actions = {EntityPolicyAction.READ})
-    void contactsEntity();
-
-    @EntityPolicy(entityClass = Address.class, actions = {EntityPolicyAction.READ})
-    void addressEntity();
-
-    @EntityPolicy(entityClass = Education.class, actions = {EntityPolicyAction.READ})
-    void educationEntity();
-
-    @EntityPolicy(entityClass = AdministrativeViolation.class, actions = {EntityPolicyAction.READ})
-    void administrativeViolationEntity();
-
-    @EntityPolicy(entityClass = CriminalViolation.class, actions = {EntityPolicyAction.READ})
-    void criminalViolationEntity();
-
-    @EntityPolicy(entityClass = Event.class, actions = {EntityPolicyAction.READ})
-    void eventEntity();
-
-
     @EntityAttributePolicy(
             entityClass = User.class,
-            attributes = {"firstName", "lastName", "patronymic", "dateOfBirth"},
+            attributes = {
+                    "id",
+                    "username",
+                    "password",
+                    "firstName",
+                    "lastName",
+                    "patronymic",
+                    "dateOfBirth",
+                    "serviceInfo",
+                    "contactsInfo",
+                    "education",
+                    "armyService",
+                    "vehicleInfo",
+                    "gender",
+                    "authorities"
+            },
             action = EntityAttributePolicyAction.MODIFY
     )
-    void userCommonFieldsView();
-
-    @EntityAttributePolicy(
-            entityClass = User.class,
-            attributes = {"username", "password"},
-            action = EntityAttributePolicyAction.MODIFY
-    )
-    void userCredentials();
+    @EntityPolicy(entityClass = User.class, actions = EntityPolicyAction.ALL)
+    void userEntityFullAccess();
 
     @EntityAttributePolicy(
             entityClass = ServiceInfo.class,
-            attributes = {"*"},
-            action = EntityAttributePolicyAction.VIEW
-    )
-    void serviceInfoSelfFields();
-
-    @EntityAttributePolicy(
-            entityClass = ServiceInfo.class,
-            attributes = {"medicalExamination"},
+            attributes = {
+                    "id",
+                    "user",
+                    "department",
+                    "rank",
+                    "status",
+                    "post",
+                    "idCard",
+                    "token",
+                    "breastplate",
+                    "startDate",
+                    "startOfPost",
+                    "penalty",
+                    "incentive",
+                    "shifts",
+                    "vocations",
+                    "medicalExamination",
+                    "qualificationClass",
+                    "vacationDaysEntitled",
+                    "vacationDaysAvailable",
+                    "vacationDaysUsed"
+            },
             action = EntityAttributePolicyAction.MODIFY
     )
-    void serviceInfoMedicalExamination();
+    @EntityPolicy(entityClass = ServiceInfo.class, actions = EntityPolicyAction.ALL)
+    void serviceInfoFullAccess();
 
     @EntityAttributePolicy(
             entityClass = IdCard.class,
-            attributes = {"*"},
+            attributes = {"id", "issued", "spl", "until"},
             action = EntityAttributePolicyAction.MODIFY
     )
-    void idCardSelfFields();
+    @EntityPolicy(entityClass = IdCard.class, actions = EntityPolicyAction.ALL)
+    void idCardFullAccess();
 
     @EntityAttributePolicy(
             entityClass = Penalty.class,
-            attributes = {"*"},
-            action = EntityAttributePolicyAction.VIEW
+            attributes = {
+                    "id",
+                    "date",
+                    "description",
+                    "initiator",
+                    "orderNumber",
+                    "penaltyStatus",
+                    "penaltyType",
+                    "userServiceInfo"
+            },
+            action = EntityAttributePolicyAction.MODIFY
     )
-    void penaltySelfFields();
+    @EntityPolicy(entityClass = Penalty.class, actions = EntityPolicyAction.ALL)
+    void penaltyFullAccess();
 
     @EntityAttributePolicy(
             entityClass = Incentive.class,
-            attributes = "*",
-            action = EntityAttributePolicyAction.VIEW)
-    void incentiveSelfFields();
+            attributes = {
+                    "id",
+                    "date",
+                    "description",
+                    "incentiveType",
+                    "initiator",
+                    "orderNumber",
+                    "userServiceInfo"
+            },
+            action = EntityAttributePolicyAction.MODIFY
+    )
+    @EntityPolicy(entityClass = Incentive.class, actions = EntityPolicyAction.ALL)
+    void incentiveFullAccess();
 
     @EntityAttributePolicy(
             entityClass = Shift.class,
-            attributes = "*",
-            action = EntityAttributePolicyAction.VIEW
+            attributes = {
+                    "id",
+                    "date",
+                    "departmentToday",
+                    "typeOfShift",
+                    "number",
+                    "startTime",
+                    "endTime",
+                    "units",
+                    "countOfStatements",
+                    "countOfClaims",
+                    "ibdWithoutMigrant",
+                    "ibdWithMigrant",
+                    "administrativeViolations",
+                    "criminalViolations"
+            },
+            action = EntityAttributePolicyAction.MODIFY
     )
-    void shiftSelfFields();
+    @EntityPolicy(entityClass = Shift.class, actions = EntityPolicyAction.ALL)
+    void shiftFullAccess();
 
     @EntityAttributePolicy(
             entityClass = Vocation.class,
-            attributes = "*",
-            action = EntityAttributePolicyAction.VIEW)
-    void vocationTypeSelfFields();
+            attributes = {
+                    "id",
+                    "userServiceInfo",
+                    "typeId",
+                    "type",
+                    "startDate",
+                    "endDate",
+                    "countOfDays",
+                    "hasDeparture",
+                    "cityToDrive",
+                    "daysAddedByDeparture"
+            },
+            action = EntityAttributePolicyAction.MODIFY
+    )
+    @EntityPolicy(entityClass = Vocation.class, actions = EntityPolicyAction.ALL)
+    void vocationFullAccess();
 
     @EntityAttributePolicy(
             entityClass = Contacts.class,
-            attributes = "*",
-            action = EntityAttributePolicyAction.VIEW
+            attributes = {
+                    "id",
+                    "user",
+                    "phoneNumber",
+                    "registration",
+                    "habitation",
+                    "nearestMetroStation"
+            },
+            action = EntityAttributePolicyAction.MODIFY
     )
-    void contactsTypeSelfFields();
+    @EntityPolicy(entityClass = Contacts.class, actions = EntityPolicyAction.ALL)
+    void contactsFullAccess();
 
     @EntityAttributePolicy(
             entityClass = Address.class,
-            attributes = "*",
-            action = EntityAttributePolicyAction.VIEW
+            attributes = {
+                    "id",
+                    "index",
+                    "city",
+                    "street",
+                    "houseNumber",
+                    "body",
+                    "flat",
+                    "typeOfHousing",
+                    "statusOfHousing"
+            },
+            action = EntityAttributePolicyAction.MODIFY
     )
-    void addressTypeSelfFields();
+    @EntityPolicy(entityClass = Address.class, actions = EntityPolicyAction.ALL)
+    void addressFullAccess();
 
     @EntityAttributePolicy(
             entityClass = Education.class,
-            attributes = "*",
-            action = EntityAttributePolicyAction.VIEW
+            attributes = {
+                    "id",
+                    "status",
+                    "type",
+                    "nameOfInstitution",
+                    "started",
+                    "until"
+            },
+            action = EntityAttributePolicyAction.MODIFY
     )
-    void educationTypeSelfFields();
+    @EntityPolicy(entityClass = Education.class, actions = EntityPolicyAction.ALL)
+    void educationFullAccess();
 
     @EntityAttributePolicy(
             entityClass = AdministrativeViolation.class,
-            attributes = "*",
-            action = EntityAttributePolicyAction.VIEW
+            attributes = {"id", "impact", "shift", "article"},
+            action = EntityAttributePolicyAction.MODIFY
     )
-    void administrativeViolationSelfFields();
+    @EntityPolicy(entityClass = AdministrativeViolation.class, actions = EntityPolicyAction.ALL)
+    void administrativeViolationFullAccess();
 
     @EntityAttributePolicy(
             entityClass = CriminalViolation.class,
-            attributes = "*",
-            action = EntityAttributePolicyAction.VIEW
+            attributes = {"id", "impact", "shift", "type"},
+            action = EntityAttributePolicyAction.MODIFY
     )
-    void criminalViolationSelfFields();
+    @EntityPolicy(entityClass = CriminalViolation.class, actions = EntityPolicyAction.ALL)
+    void criminalViolationFullAccess();
 
     @EntityAttributePolicy(
             entityClass = Event.class,
-            attributes = "*",
-            action = EntityAttributePolicyAction.VIEW
+            attributes = {"id", "name", "description", "eventType", "date", "time", "place", "shiftOfDepartment"},
+            action = EntityAttributePolicyAction.MODIFY
     )
-    void eventSelfFields();
+    @EntityPolicy(entityClass = Event.class, actions = EntityPolicyAction.ALL)
+    void eventFullAccess();
+
+    @EntityAttributePolicy(
+            entityClass = DeletedEvent.class,
+            attributes = {
+                    "id",
+                    "name",
+                    "description",
+                    "eventType",
+                    "date",
+                    "time",
+                    "place",
+                    "shiftOfDepartment",
+                    "originalEventId"
+            },
+            action = EntityAttributePolicyAction.MODIFY
+    )
+    @EntityPolicy(entityClass = DeletedEvent.class, actions = EntityPolicyAction.ALL)
+    void deletedEventFullAccess();
+
+    @EntityAttributePolicy(
+            entityClass = Vehicle.class,
+            attributes = {
+                    "id",
+                    "user",
+                    "brand",
+                    "model",
+                    "stateNumber",
+                    "registrationCertificate",
+                    "insurance"
+            },
+            action = EntityAttributePolicyAction.MODIFY
+    )
+    @EntityPolicy(entityClass = Vehicle.class, actions = EntityPolicyAction.ALL)
+    void vehicleFullAccess();
+
+    @EntityAttributePolicy(
+            entityClass = Department.class,
+            attributes = {"id", "number", "serviceInfos"},
+            action = EntityAttributePolicyAction.MODIFY
+    )
+    @EntityPolicy(entityClass = Department.class, actions = EntityPolicyAction.ALL)
+    void departmentFullAccess();
 
     @ViewPolicy(viewIds = {
             "MainView",
@@ -190,15 +297,20 @@ public interface PolicemanRole {
             "MyShift.list",
             "Event.list",
             "LastEvent.list",
-            "LastEvent.detail"
-//            "CompensatoryTimeRaportView"
-//            "Event.detail",
-//            "DeletedEvent.detail",
-//            "DeletedEvent.list"
+            "LastEvent.detail",
+            "Event.detail",
+            "DeletedEvent.detail",
+            "DeletedEvent.list",
+            "Vehicle.list",
+            "Vehicle.detail",
+            "Department.list",
+            "MainViewTopMenu",
+            "CompensatoryTimeRaportView"
     })
     void userViews();
 
     @MenuPolicy(menuIds = {
+            "my_data",
             "all_employees_to_read",
             "my_profile",
             "my_shifts",
@@ -206,6 +318,9 @@ public interface PolicemanRole {
             "my_criminal_violations",
             "my_penalties",
             "my_incentives",
+            "application",
+            "events",
+            "raports",
             "all_vocations",
             "all_administrative_violations",
             "all_criminal_violations",
@@ -214,8 +329,16 @@ public interface PolicemanRole {
             "CriminalViolation.list",
             "future_events",
             "last_events",
-            "menuAllShifts"
-//            "CompensatoryTimeRaportView"
+            "deleted_events",
+            "menuAllShifts",
+            "User.list",
+            "Penalty.list",
+            "Incentive.list",
+            "DeletedEvent.list",
+            "CompensatoryTimeRaportView"
     })
     void userMenu();
+
+    @SpecificPolicy(resources = "ui.loginToUi")
+    void loginToUi();
 }
